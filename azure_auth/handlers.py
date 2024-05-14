d4ce9bb805322ef02dbc40fa0db78338c31f6c15
Sebastian Muthwill <sebastian@sebserver.de>
2024-05-14 09:59:54 +0200

    use userPrincipalName for django username

--- azure_auth/handlers.py
+++ azure_auth/handlers.py
@@ -98,7 +98,7 @@ class AuthHandler:
         try:
             user = UserModel._default_manager.get_by_natural_key(email)  # type: ignore
         except UserModel.DoesNotExist:
-            user = UserModel._default_manager.create_user(username=email, email=email)  # type: ignore
+            user = UserModel._default_manager.create_user(username=azure_user["userPrincipalName"], email=email)  # type: ignore
             user.first_name = attr if (attr := azure_user["givenName"]) else ""
             user.last_name = attr if (attr := azure_user["surname"]) else ""
             user.save()
